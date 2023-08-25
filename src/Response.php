3ee7c999b3aaafa3f87b589bf39f55caf5a83ebb
Oseias Isidoro <oseiasisidoro@gmail.com>
2023-08-25 03:31:20 -0300

    creates Response.php

---
+++ src/Response.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace IstpaySDK\SDK;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+
+abstract class Response
+{
+    protected ResponseInterface $response;
+    public int $http_code;
+    protected array $errors = [];
+
+    public function __construct(ResponseInterface $response)
+    {
+        $this->response = $response;
+        $this->http_code = $response->getStatusCode();
+    }
+
+    public function getRawResponse(): StreamInterface
+    {
+        return $this->response->getBody();
+    }
+
+    public function success(): bool
+    {
+        return ($this->http_code >= 200 and $this->http_code <= 299);
+    }
+
+    public function responseToArray()
+    {
+        return json_decode($this->getRawResponse(), true);
+    }
+
+    public function responseToObject()
+    {
+        return json_decode($this->getRawResponse());
+    }
+
+    public function clientError(): bool
+    {
+        return ($this->http_code >= 400 and $this->http_code <= 499);
+    }
+
+    public function serverError(): bool
+    {
+        return ($this->http_code >= 500 and $this->http_code <= 599);
+    }
+
+    public function getErrors(): array
+    {
+        $response_decoded = $this->responseToArray();
+
+        if ($this->clientError())
+        {
+            foreach ($response_decoded['errors'] as $value)
+            {
+                $this->addMsgError($value[0]);
+            }
+        } else if ($this->serverError())
+        {
+            $this->addMsgError($this->getRawResponse());
+        }
+
+        return $this->errors;
+    }
+
+    protected function addMsgError(string $msg)
+    {
+        $this->errors[] = $msg;
+    }
+}
